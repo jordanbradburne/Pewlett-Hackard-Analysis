98e86a460dbc698f233c19f842c301a7a4803658
jordanbradburne <85847344+jordanbradburne@users.noreply.github.com>
2021-08-08 14:52:58 -0700

    Updated Code

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -17,12 +17,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -36,26 +36,28 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-    from_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	emp_no INT NOT NULL,
+	dept_no varchar(4) NOT NULL,
+	from_date date,
+	to_date date,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE Titles (
-    emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
+
 
 SELECT * FROM departments;
 SELECT * FROM employees;
 SELECT * FROM dept_manager;
 SELECT * FROM salaries;
 SELECT * FROM dept_emp;
-SELECT * FROM Titles;
+SELECT * FROM titles;
